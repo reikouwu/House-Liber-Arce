7409cade42ba590701f268a9d48b4e8e12b41d6d
nicoleiii <nagitookomaeda100@gmail.com>
2026-01-20 17:22:21 -0500

    Edit

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, Integer, String
 from app.db import Base
-
+# ----------------------------
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
+
